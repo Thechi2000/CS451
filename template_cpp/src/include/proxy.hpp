dccc6137e53ad54844cc76d8b8c0688a7e15839a
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-21 15:10:37 +0100

    refactor: split proxy between hpp and tpp

--- template_cpp/src/include/proxy.hpp
+++ template_cpp/src/include/proxy.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "serde.hpp"
-#include <algorithm>
+#include <parser.hpp>
+#include <serde.hpp>
+#include <udp.hpp>
+
 #include <chrono>
 #include <cstddef>
 #include <functional>
 #include <map>
-#include <optional>
-#include <parser.hpp>
-#include <udp.hpp>
 #include <vector>
 
 template <typename Payload> class Proxy {
@@ -18,101 +17,16 @@ template <typename Payload> class Proxy {
         Payload content;
     };
 
-    Proxy(const Host &host)
-        : seq_(1), received_(config.hosts().size(),
-                             DeliveredEntry{1, std::map<u32, Message>()}),
-          sent_(config.hosts().size()), lastSend_(Clock::now()), socket(host) {
-        for (size_t i = 0; i < config.hosts().size() + 1; ++i) {
-            received_.emplace_back();
-        }
-    }
-    ~Proxy() {}
+    Proxy(const Host &host);
+    ~Proxy();
 
-    void send(const Message &m, const Host &host);
-    void send(const Payload &p, const Host &host) {
-        u32 seq = seq_++;
-
-        void *buffer = malloc(UDP_PACKET_MAX_SIZE);
-        size_t size =
-            serialize(Message{seq, p}, reinterpret_cast<u8 *>(buffer));
-
-        sent_[host.id - 1].insert({seq, {buffer, size, false}});
-
-        socket.sendTo(buffer, size, host);
-    }
-    void send(const std::vector<Payload> &payloads, const Host &host) {
-        std::vector<ToSend> messages(payloads.size());
-        for (size_t i = 0; i < payloads.size(); ++i) {
-            auto &to_send = messages[i];
-            Message msg = {seq_++, payloads[i]};
-
-            void *buffer = malloc(UDP_PACKET_MAX_SIZE);
-            size_t size = serialize(msg, reinterpret_cast<u8 *>(buffer));
-
-            to_send = {buffer, size, false};
-
-            sent_[host.id - 1].insert({msg.seq, to_send});
-        }
-
-        innerSend(messages, host);
-    }
+    void send(const Payload &p, const Host &host);
+    void send(const std::vector<Payload> &payloads, const Host &host);
 
     using Callback = std::function<void(Message &message, const Host &host)>;
     void setCallback(Callback cb) { callback_ = cb; }
 
-    void wait() {
-        u8 buffer[UDP_PACKET_MAX_SIZE] = {0};
-
-        Host host;
-        std::optional<Message> msg = std::nullopt;
-        while (true) {
-            if (Clock::now() - lastSend_ > TIMEOUT) {
-                for (size_t hostIdx = 0; hostIdx < config.hosts().size();
-                     hostIdx++) {
-                    if (sent_[hostIdx].size() == 0) {
-                        continue;
-                    }
-
-                    std::vector<ToSend> messages(sent_[hostIdx].size());
-
-                    size_t i = 0;
-                    for (const auto &entry : sent_[hostIdx]) {
-                        messages[i] = entry.second;
-                        i++;
-                    }
-
-                    innerSend(messages, config.host(hostIdx + 1));
-                }
-                lastSend_ = Clock::now();
-            }
-
-            size_t size = socket.recvFrom(buffer, UDP_PACKET_MAX_SIZE, host);
-            size_t processedBytes = 0;
-
-            if (size > 0) {
-                u8 acks[8 * ACK_SIZE];
-                size_t acksSize = 0;
-
-                host.id = static_cast<u32>(
-                              std::find_if(config.hosts().begin(),
-                                           config.hosts().end(),
-                                           [&](const Host &e) {
-                                               return e.ip == host.ip &&
-                                                      e.port == host.port;
-                                           }) -
-                              config.hosts().begin()) +
-                          1;
-
-                while (size > processedBytes) {
-                    processedBytes +=
-                        handleMessage(buffer + processedBytes, host,
-                                      acks + acksSize, acksSize);
-                }
-
-                socket.sendTo(acks, acksSize, host);
-            }
-        }
-    }
+    void wait();
 
   private:
     struct ToSend {
@@ -132,106 +46,16 @@ template <typename Payload> class Proxy {
     const static size_t MSG_META_SIZE = 9;
     const static size_t ACK_SIZE = sizeof(Ack) + 1;
 
-    void innerSend(const std::vector<ToSend> &payloads, const Host &host) {
-        u8 buffer[UDP_PACKET_MAX_SIZE];
-
-        for (auto it = payloads.begin(); it != payloads.end();) {
-            size_t size = 0;
-            for (int i = 0; i < 8 && it != payloads.end(); ++i) {
-
-                if (size + it->length > UDP_PACKET_MAX_SIZE) {
-                    break;
-                }
-
-                memcpy(buffer + size, it->message, it->length);
-
-                size += it->length;
-                it++;
-            }
-
-            socket.sendTo(buffer, size, host);
-        }
-    }
+    void innerSend(const std::vector<ToSend> &payloads, const Host &host);
 
     using Clock = std::chrono::system_clock;
     const Clock::duration TIMEOUT = Clock::duration(10000000); // 10ms
 
-    size_t serialize(const Message &msg, u8 *buff) {
-        size_t size = 5;
-
-        buff = write_byte(buff, 0);
-        buff = write_u32(buff, msg.seq);
-        buff = ser(msg.content, buff, size);
-
-        return size;
-    }
-    size_t serialize(const Ack &ack, u8 *buff) {
-        buff = write_byte(buff, 1);
-        buff = write_u32(buff, ack.seq);
-
-        return ACK_SIZE;
-    }
+    size_t serialize(const Message &msg, u8 *buff);
+    size_t serialize(const Ack &ack, u8 *buff);
 
     size_t handleMessage(u8 *buff, const Host &host, u8 *acks,
-                         size_t &acksSize) {
-        u8 type;
-        buff = read_byte(buff, type);
-
-        if (type == 0) {
-            Message b;
-            size_t processed_size = MSG_META_SIZE;
-            buff = read_u32(buff, b.seq);
-            buff = deserialize(b.content, buff, processed_size);
-
-            Ack d = Ack{b.seq};
-            acksSize += serialize(d, acks);
-
-            auto &deliveredEntry = received_[host.id - 1];
-
-            if (d.seq < deliveredEntry.lowerBound ||
-                deliveredEntry.delivered.count(d.seq) > 0) {
-                return processed_size;
-            }
-
-            if (d.seq == deliveredEntry.lowerBound) {
-                deliveredEntry.lowerBound++;
-
-                callback_(b, host);
-
-                auto begin = deliveredEntry.delivered.begin();
-                auto it = begin;
-
-                if (it->first == deliveredEntry.lowerBound) {
-                    do {
-                        callback_(it->second, host);
-                        deliveredEntry.lowerBound++;
-                    } while ((++it)->first == deliveredEntry.lowerBound);
-
-                    deliveredEntry.delivered.erase(begin, it);
-                }
-            } else {
-                deliveredEntry.delivered.insert({d.seq, b});
-            }
-
-            return processed_size;
-
-        } else {
-            Ack b;
-            buff = read_u32(buff, b.seq);
-
-            if (sent_[host.id - 1].count(b.seq) > 0) {
-                auto &entry = sent_[host.id - 1][b.seq];
-                entry.acked = true;
-
-                if (entry.acked) {
-                    free(sent_[host.id - 1][b.seq].message);
-                    sent_[host.id - 1].erase(b.seq);
-                }
-            }
-
-            return ACK_SIZE;
-        }
-    }
+                         size_t &acksSize);
 
     u32 seq_;
 
@@ -243,3 +67,5 @@ template <typename Payload> class Proxy {
 
     UdpSocket socket;
 };
+
+#include "../src/proxy.tpp"
---
+++ template_cpp/src/src/proxy.tpp
@@ -0,0 +1,201 @@
+#include <proxy.hpp>
+
+#include <algorithm>
+#include <optional>
+
+template <typename Payload>
+Proxy<Payload>::Proxy(const Host &host)
+    : seq_(1), received_(config.hosts().size(),
+                         DeliveredEntry{1, std::map<u32, Message>()}),
+      sent_(config.hosts().size()), lastSend_(Clock::now()), socket(host) {
+    for (size_t i = 0; i < config.hosts().size() + 1; ++i) {
+        received_.emplace_back();
+    }
+}
+template <typename Payload> Proxy<Payload>::~Proxy() {}
+
+template <typename Payload>
+void Proxy<Payload>::send(const Payload &p, const Host &host) {
+    u32 seq = seq_++;
+
+    void *buffer = malloc(UDP_PACKET_MAX_SIZE);
+    size_t size = serialize(Message{seq, p}, reinterpret_cast<u8 *>(buffer));
+
+    sent_[host.id - 1].insert({seq, {buffer, size, false}});
+
+    socket.sendTo(buffer, size, host);
+}
+template <typename Payload>
+void Proxy<Payload>::send(const std::vector<Payload> &payloads,
+                          const Host &host) {
+    std::vector<ToSend> messages(payloads.size());
+    for (size_t i = 0; i < payloads.size(); ++i) {
+        auto &to_send = messages[i];
+        Message msg = {seq_++, payloads[i]};
+
+        void *buffer = malloc(UDP_PACKET_MAX_SIZE);
+        size_t size = serialize(msg, reinterpret_cast<u8 *>(buffer));
+
+        to_send = {buffer, size, false};
+
+        sent_[host.id - 1].insert({msg.seq, to_send});
+    }
+
+    innerSend(messages, host);
+}
+
+template <typename Payload> void Proxy<Payload>::wait() {
+    u8 buffer[UDP_PACKET_MAX_SIZE] = {0};
+
+    Host host;
+    std::optional<Message> msg = std::nullopt;
+    while (true) {
+        if (Clock::now() - lastSend_ > TIMEOUT) {
+            for (size_t hostIdx = 0; hostIdx < config.hosts().size();
+                 hostIdx++) {
+                if (sent_[hostIdx].size() == 0) {
+                    continue;
+                }
+
+                std::vector<ToSend> messages(sent_[hostIdx].size());
+
+                size_t i = 0;
+                for (const auto &entry : sent_[hostIdx]) {
+                    messages[i] = entry.second;
+                    i++;
+                }
+
+                innerSend(messages, config.host(hostIdx + 1));
+            }
+            lastSend_ = Clock::now();
+        }
+
+        size_t size = socket.recvFrom(buffer, UDP_PACKET_MAX_SIZE, host);
+        size_t processedBytes = 0;
+
+        if (size > 0) {
+            u8 acks[8 * ACK_SIZE];
+            size_t acksSize = 0;
+
+            host.id =
+                static_cast<u32>(std::find_if(config.hosts().begin(),
+                                              config.hosts().end(),
+                                              [&](const Host &e) {
+                                                  return e.ip == host.ip &&
+                                                         e.port == host.port;
+                                              }) -
+                                 config.hosts().begin()) +
+                1;
+
+            while (size > processedBytes) {
+                processedBytes += handleMessage(buffer + processedBytes, host,
+                                                acks + acksSize, acksSize);
+            }
+
+            socket.sendTo(acks, acksSize, host);
+        }
+    }
+}
+
+template <typename Payload>
+void Proxy<Payload>::innerSend(const std::vector<ToSend> &payloads,
+                               const Host &host) {
+    u8 buffer[UDP_PACKET_MAX_SIZE];
+
+    for (auto it = payloads.begin(); it != payloads.end();) {
+        size_t size = 0;
+        for (int i = 0; i < 8 && it != payloads.end(); ++i) {
+
+            if (size + it->length > UDP_PACKET_MAX_SIZE) {
+                break;
+            }
+
+            memcpy(buffer + size, it->message, it->length);
+
+            size += it->length;
+            it++;
+        }
+
+        socket.sendTo(buffer, size, host);
+    }
+}
+
+template <typename Payload>
+size_t Proxy<Payload>::serialize(const Message &msg, u8 *buff) {
+    size_t size = 5;
+
+    buff = write_byte(buff, 0);
+    buff = write_u32(buff, msg.seq);
+    buff = ser(msg.content, buff, size);
+
+    return size;
+}
+template <typename Payload>
+size_t Proxy<Payload>::serialize(const Ack &ack, u8 *buff) {
+    buff = write_byte(buff, 1);
+    buff = write_u32(buff, ack.seq);
+
+    return ACK_SIZE;
+}
+
+template <typename Payload>
+size_t Proxy<Payload>::handleMessage(u8 *buff, const Host &host, u8 *acks,
+                                     size_t &acksSize) {
+    u8 type;
+    buff = read_byte(buff, type);
+
+    if (type == 0) {
+        Message b;
+        size_t processed_size = MSG_META_SIZE;
+        buff = read_u32(buff, b.seq);
+        buff = deserialize(b.content, buff, processed_size);
+
+        Ack d = Ack{b.seq};
+        acksSize += serialize(d, acks);
+
+        auto &deliveredEntry = received_[host.id - 1];
+
+        if (d.seq < deliveredEntry.lowerBound ||
+            deliveredEntry.delivered.count(d.seq) > 0) {
+            return processed_size;
+        }
+
+        if (d.seq == deliveredEntry.lowerBound) {
+            deliveredEntry.lowerBound++;
+
+            callback_(b, host);
+
+            auto begin = deliveredEntry.delivered.begin();
+            auto it = begin;
+
+            if (it->first == deliveredEntry.lowerBound) {
+                do {
+                    callback_(it->second, host);
+                    deliveredEntry.lowerBound++;
+                } while ((++it)->first == deliveredEntry.lowerBound);
+
+                deliveredEntry.delivered.erase(begin, it);
+            }
+        } else {
+            deliveredEntry.delivered.insert({d.seq, b});
+        }
+
+        return processed_size;
+
+    } else {
+        Ack b;
+        buff = read_u32(buff, b.seq);
+
+        if (sent_[host.id - 1].count(b.seq) > 0) {
+            auto &entry = sent_[host.id - 1][b.seq];
+            entry.acked = true;
+
+            if (entry.acked) {
+                free(sent_[host.id - 1][b.seq].message);
+                sent_[host.id - 1].erase(b.seq);
+            }
+        }
+
+        return ACK_SIZE;
+    }
+}
